79b93c390aa62ce4605a83123ad293e0d7c839ce
Klaudia Kedra <kkedra@ucvts.tec.nj.us>
2019-03-26 10:52:12 -0400

    almost there

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 function check() {
 
-  var question1 = document.quiz.question1;
+  var question1 = document.quiz.question1.value;
   var question2 = document.quiz.question2.value;
   var question3 = document.quiz.question3.value;
   var question4 = document.quiz.question4.value;
@@ -10,57 +10,115 @@ function check() {
   var question8 = document.quiz.question8.value;
   var question9 = document.quiz.question9.value;
   var question10 = document.quiz.question10.value;
+  var question11 = document.quiz.question1.value;
+  var question12 = document.quiz.question2.value;
+  var question13 = document.quiz.question3.value;
+  var question14 = document.quiz.question4.value;
+  var question15 = document.quiz.question5.value;
+  var question16 = document.quiz.question5.value;
+  var question17 = document.quiz.question5.value;
+  var question18 = document.quiz.question5.value;
+  var question19 = document.quiz.question5.value;
+  var question20 = document.quiz.question5.value;
   var correct = 0;
 
-
-  if (question1 == "Cinderella"){
+question1 = question1.toUpperCase();
+question7 = question7.toUpperCase();
+question9 = question9.toUpperCase();
+question10 = question10.toUpperCase();
+  if (question1 == "RAPUNZEL"){
     correct++;
   }
-  if (question2.value == 1){
+  if (question2 == 1){
     correct++;
   }
-  if (question3.value == 1){
+  if (question3 == 1){
     correct++;
   }
-  if (question4.value == 1){
+  if (question4 == 1){
       correct++;
   }
-  if (question5.value == 1){
+  if (question5 == 1){
     correct++;
   }
-  if (question6.value == 1){
+  if (question6 == 1){
     correct++;
   }
-  if (question7 == "Rapunzel"){
+  if (question7 == "RAPUNZEL"){
     correct++;
   }
-  if (question8.value == 1){
+  if (question8 == 1){
     correct++;
   }
-  if (question9 == "Snow White"){
+  if (question9 == "SNOW WHITE"){
     correct++;
   }
-  if (question10 == "Olaf"){
+  if (question10 == "OLAF"){
     correct++;
   }
-  var messages = ["Great job!", "That's okay.", "Better luck next time"];
+  if (question11 == 1){
+    correct++;
+  }
+  if (question12 == 1){
+    correct++;
+  }
+  if (question13 == 1){
+    correct++;
+  }
+  if (question14 == 1){
+    correct++;
+  }
+  if (question15 == 1){
+    correct++;
+  }
+  if (question16 == 1){
+    correct++;
+  }
+  if (question17 == 1){
+    correct++;
+  }
+  if (question18 == 1){
+    correct++;
+  }
+  if (question19 == 1){
+    correct++;
+  }
+  if (question20 == 1){
+    correct++;
+  }
+
+
+
+  var messages = ["Excellent!", "Great job!", "Good job!", "Good effort.", "Better luck next time."];
 
   var range;
 
-  if (correct < 2) {
-    range = 2;
-  }
-  if (correct > 3 && correct < 7) {
-    range = 1;
-  }
-  if (correct > 7) {
-    range = 0;
-  }
-
+if (correct <= 5) {
+  range = 4;
+} else if (correct < 9) {
+  range = 3;
+} else if (correct < 12) {
+  range = 2;
+} else if (correct < 16) {
+  range = 1;
+} else {
+  range = 0;
+}
 
 document.getElementById("afterSubmit").style.visibility = "visible";
 
 document.getElementById("message").innerHTML = messages [range];
 document.getElementById("numberCorrect").innerHTML = "You got " + correct + " correct.";
 
+}
+
+function reset() {
+  mc = document.getElementsByClassName("mc").checked;
+  for (let x = 0; x < mc.length; x++) {
+    mc[0] = false;
+  }
+  textbox = document.getElementsByClassName("textbox").checked;
+  for (let x = 0; x < textbox.length; x++) {
+    textbox[0] = "";
+  }
 }
