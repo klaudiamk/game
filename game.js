e1972ff6cccb502370fe6a1ec33a581a56f5a70d
Klaudia Kedra <kkedra@ucvts.tec.nj.us>
2019-04-03 10:25:06 -0400

    finished

--- game.js
+++ game.js
@@ -1,114 +1,83 @@
-function check() {
 
-  var question1 = document.quiz.question1.value;
-  var question2 = document.quiz.question2.value;
-  var question3 = document.quiz.question3.value;
-  var question4 = document.quiz.question4.value;
-  var question5 = document.quiz.question5.value;
-  var question6 = document.quiz.question6.value;
-  var question7 = document.quiz.question7.value;
-  var question8 = document.quiz.question8.value;
-  var question9 = document.quiz.question9.value;
-  var question10 = document.quiz.question10.value;
-  var question11 = document.quiz.question11.value;
-  var question12 = document.quiz.question12.value;
-  var question13 = document.quiz.question13.value;
-  var question14 = document.quiz.question14.value;
-  var question15 = document.quiz.question15.value;
-  var question16 = document.quiz.question16.value;
-  var question17 = document.quiz.question17.value;
-  var question18 = document.quiz.question18.value;
-  var question19 = document.quiz.question19.value;
-  var question20 = document.quiz.question20.value;
-  var correct = 0;
+let correct = 0;
+let question = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-question1 = question1.toUpperCase();
-question7 = question7.toUpperCase();
-question9 = question9.toUpperCase();
-question10 = question10.toUpperCase();
+function editValues() {
 
-  if (question1 == "RAPUNZEL"){
-    correct++;
-  }
-  if (question2 == 1){
-    correct++;
-  }
-  if (question3 == 1){
-    correct++;
-  }
-  if (question4 == 1){
-      correct++;
-  }
-  if (question5 == 1){
-    correct++;
-  }
-  if (question6 == 1){
-    correct++;
-  }
-  if (question7 == "RAPUNZEL"){
-    correct++;
-  }
-  if (question8 == 1){
-    correct++;
-  }
-  if (question9 == "SNOW WHITE"){
-    correct++;
-  }
-  if (question10 == "OLAF"){
-    correct++;
-  }
-  if (question11 == 1){
-    correct++;
-  }
-  if (question12 == 1){
-    correct++;
-  }
-  if (question13 == 1){
-    correct++;
-  }
-  if (question14 == 1){
-    correct++;
-  }
-  if (question15 == 1){
-    correct++;
-  }
-  if (question16 == 1){
-    correct++;
-  }
-  if (question17 == 1){
-    correct++;
-  }
-  if (question18 == 1){
-    correct++;
-  }
-  if (question19 == 1){
-    correct++;
-  }
-  if (question20 == 1){
-    correct++;
-  }
+  correct = 0;
+  question[1] = document.quiz.question1.value;
+  question[2] = document.quiz.question2.value;
+  question[3] = document.quiz.question3.value;
+  question[4] = document.quiz.question4.value;
+  question[5] = document.quiz.question5.value;
+  question[6] = document.quiz.question6.value;
+  question[7] = document.quiz.question7.value;
+  question[8] = document.quiz.question8.value;
+  question[9] = document.quiz.question9.value;
+  question[10] = document.quiz.question10.value;
+  question[11] = document.quiz.question11.value;
+  question[12] = document.quiz.question12.value;
+  question[13] = document.quiz.question13.value;
+  question[14] = document.quiz.question14.value;
+  question[15] = document.quiz.question15.value;
+  question[16] = document.quiz.question16.value;
+  question[17] = document.quiz.question17.value;
+  question[18] = document.quiz.question18.value;
+  question[19] = document.quiz.question19.value;
+  question[20] = document.quiz.question20.value;
 
-
-
-  var messages = ["Excellent!", "Great job!", "Good job!", "Good effort.", "Better luck next time."];
-
-  var range;
-
-if (correct <= 5) {
-  range = 4;
-} else if (correct < 9) {
-  range = 3;
-} else if (correct < 12) {
-  range = 2;
-} else if (correct < 16) {
-  range = 1;
-} else {
-  range = 0;
 }
 
-document.getElementById("afterSubmit").style.visibility = "visible";
+function check() {
 
-document.getElementById("message").innerHTML = messages [range];
-document.getElementById("numberCorrect").innerHTML = "You got " + correct + " correct.";
+  question[1] = question[1].toUpperCase();
+  question[7] = question[7].toUpperCase();
+  question[9] = question[9].toUpperCase();
+  question[10] = question[10].toUpperCase();
+  if (question[1] == "RAPUNZEL"){
+    question[1] = 1;
+  }
+  if (question[7] == "RAPUNZEL"){
+    question[7] = 1;
+  }
+  if (question[9] == "SNOW WHITE"){
+    question[9] = 1;
+  }
+  if (question[10] == "OLAF"){
+    question[10] = 1;
+  }
 
+  for (let x = 1; x <= 20; x++) {
+    if (question[x] == 1) {
+      correct++
+    }
+  }
+
+}
+
+function right() {
+
+editValues();
+check();
+
+    var messages = ["Excellent!", "Great job!", "Good job!", "Good effort.", "Better luck next time."];
+
+    var range;
+
+  if (correct <= 5) {
+    range = 4;
+  } else if (correct < 9) {
+    range = 3;
+              } else if (correct < 12) {
+    range = 2;
+  } else if (correct < 16) {
+    range = 1;
+  } else {
+    range = 0;
+  }
+
+  document.getElementById("afterSubmit").style.visibility = "visible";
+
+  document.getElementById("message").innerHTML = messages [range];
+  document.getElementById("numberCorrect").innerHTML = "You got " + correct + " correct.";
 }
